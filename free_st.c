0b924fd44a3326ce1c8dad16bb4eef9fe1648cc8
achdaisy <oduordaizy@gmail.com>
2023-10-19 15:57:49 -0700

    free stack function

---
+++ free_st.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * free_st - frees the stack
+ * @h: head of the stack to be freed;
+ *
+ * Return: nothing
+ */
+
+void free_st(stack_t *h)
+{
+	stack_t *ptr;
+
+	ptr = h;
+	while (h)
+	{
+		ptr = h->next;
+		free(h);
+		h = ptr;
+	}
+}
