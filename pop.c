66f6d21b90695d2d75776bfc2fd2f7317ff7caf0
achdaisy <oduordaizy@gmail.com>
2023-10-21 13:45:35 -0700

    pop

---
+++ pop.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * pop_t - removes an element from the stack
+ * @head: head of the stack
+ * @incrementor: variable to hold integer values(its unused)
+ *
+ * Return: nothing
+ */
+
+void pop_t(stack_t **head, unsigned int incrementor)
+{
+	stack_t *top;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", incrementor);
+		fclose(monty.filename);
+		free(monty.buffer);
+		free_st(*head);
+		exit(EXIT_FAILURE);
+	}
+	top = *head;
+	*head = top->next;
+	free(top);
+}
