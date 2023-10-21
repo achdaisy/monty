79d171746f7f5b02239756923a1f7b6e03b829bf
achdaisy <oduordaizy@gmail.com>
2023-10-21 13:46:39 -0700

    swap function

---
+++ swap.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * swap - swaps the top elements of teh stack
+ * @head: head of the stack
+ *
+ * Return: nothing
+ */
+
+void swap_t(stack_t **head, unsigned int incrementor)
+{
+	stack_t *ptr;
+	int len = 0, number;
+
+	ptr = *head;
+	while (ptr)
+	{
+		ptr = ptr->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short", incrementor);
+		fclose(monty.filename);
+		free(monty.buffer);
+		free_st(*head);
+		exit(EXIT_FAILURE);
+	}
+	ptr = *head;
+	number = ptr->n;
+	ptr->n = ptr->next->n;
+	ptr->next->n = number;
+}	
