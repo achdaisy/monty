107faae58b6feeefcabfa210abfcf66a7bbcca9d
achdaisy <oduordaizy@gmail.com>
2023-10-21 13:39:01 -0700

    nop implementation

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * nop_t - no operation
+ * @head: head of the stack
+ * @incrementor: variable to hold integer values
+ *
+ * Return: nothing
+ */
+
+void nop_t(stack_t **head, unsigned int incrementor)
+{
+	(void) incrementor;
+	(void) head;
+}
